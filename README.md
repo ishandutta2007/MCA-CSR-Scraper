dc4b0044265d5628adef739bf7cea5b091acb856
Samuel Frantz <sfrantz@gwu.edu>
2019-01-29 22:10:14 -0500

    Input at command line; new example files; test mode

--- README.md
+++ README.md
@@ -2,14 +2,47 @@
 
 ## Description
 
-This Python script uses BeautifulSoup to scrape Article 135 disclosures from the Indian Ministry of Corporate Affairs CSR database at https://csr.gov.in/CSR/master_search.php. It will return disclosures from the 2014-15, 2015-16, and 2016-17 fiscal years.
+This Python script uses BeautifulSoup to scrape Article 135 disclosures from the Indian Ministry of Corporate Affairs (MCA) CSR database at https://csr.gov.in/CSR/master_search.php. It returns disclosures from the 2014-15, 2015-16, and 2016-17 fiscal years. I will update the script when 2017-18 disclosures are added to the MCA database.
 
 ## Usage
 
-The scraper takes a list of Company Identification Numbers (CINs) from a .csv file and queries the database for a user-defined range of these companies. For example, using the provided file `companies_15_16.csv`, defining a range of `0:99` will return the CSR disclosures of the top 100 companies, as ranked by spending in FY 2015-16. The range is currently defined within the script rather than through user input.
+The scraper takes up to two command line arguments: a mandatory argument containing a local path to a .csv file; and an optional argument to enable a test mode:
+
+`> python3.x mca_scrape.py [input] [options]`
+
+It is good practice for the user to update the `headers` variable to include their name and contact information prior to running the scraping procedure. Example:
+
+`headers = {'user-agent': 'Intel_Mac_OSX 10_14_3; John Doe/Anywhere, US; john@doe.com'}`
+
+There is a one-second delay between requests.
+
+### Input CSV Format
+The input .csv file must have a column labeled `cin`, containing Indian corporate identity numbers (CINs). Optionally, the file may also include a column labeled `company_name`, in which case the company names provided by the user will be included in the output. Any other columns in the file will be ignored.
+
+The script will pass over any CIN that is not 21 characters in length and that does not begin with L or U, since these irregular CINs are not currently included in the MCA database. Such CINs will be recorded in the exceptions file returned by the script at the end of its run.
+
+### Test mode
+Providing a `-t` option at the command line enables a test mode. Under this mode, the scraper will collect data for only the first ten CINs in the input file.
+
+### Example
+
+`> python3.7 mca_scrape.py ./examples/csr_top_100.csv -t`
 
 ## Output
 
-The scraper produces two files at the end of its run: a .csv file containing the CSR disclosures, and a .csv file listing CINs that produced exceptions during the scraping procedure. 
+The scraper produces two files at the end of its run: a .csv file containing the CSR disclosures, and a .csv file listing CINs that produced exceptions during the scraping procedure.
 
+## Included CSV and output files
 
+The `examples` folder includes three files:
+ * `csr_top_100.csv` includes CINs and company names of 100 entities, ranked by their total CSR spending from FY 2014-15 to FY 2016-17. This is based on CSR spending reported in company standalone annual reports, which I have aggregated from the ProwessDx dataset. Note that not all of these companies are included in the MCA data; in particular, those with irregular CINs are not in the database. The scraper excludes these during its run and notes their CINs as exceptions;
+
+ * `csr_top_100_results.csv` contains the results output of the script, as collected on January 29, 2019;
+
+ * `csr_top_100_exceptions.csv` contains the exceptions recorded during the script's run.
+
+## Notes, known issues, and areas for improvement:
+
+_Exceptions / connection issues_:
+* The connection to the MCA website occasionally fails, in which case an exception is recorded and the script continues. The exceptions file can be fed back into the script to collect any additional data that was missed due to connection issues. Future versions will improve the robustness of handling connection errors;
+* The exceptions file currently records only the CIN, not the company name or nature of the error.
--- mca_scrape.py
+++ mca_scrape.py
@@ -1,123 +1,171 @@
 
 # Scrapes CSR project data from Ministry of Corporate Affairs (India) website
-# Modified April 10, 2018
 # Samuel Frantz / George Washington University / sfrantz@gwu.edu
 
 import requests, bs4, time, re
 import pandas as pd
-from sys import argv
+from sys import argv, exit
 
-headers = {'user-agent': 'Intel_Mac_OSX 10_13_3; Samuel Frantz/Washington, DC sfrantz@gwu.edu'}
+# User should provide a list of CINs and an optional test mode at the command line
+if (len(argv) != 2 and len(argv) != 3):
+    print("Usage: mca_scrape.py [input] [options]")
+    exit(1)
+
+# Import list of company identification numbers (CINs) and names
+try:
+    companies = pd.read_csv(argv[1], keep_default_na=False)
+except:
+    print("Unable to read file " + str(argv[1]))
+    exit(1)
+
+# Create lists to store the data and any exceptions encountered
+csr_data=[]
+exceptions = []
+
+# List of CINs to cycle through
+
+cinList = companies['cin'].tolist()
+
+# Your info goes here
+headers = {'user-agent': 'Intel_Mac_OSX 10_14_3'}
 
 starturl='http://www.csr.gov.in/CSR/companyprofile.php?year=FY+2015-16&CIN='
 
-# Import list of company identification numbers (CINs) and names
-companies = pd.read_csv('companies_15_16.csv')
+# Set counter for accessing dataframe by index number
+# Right now this is used only to extract 'company_name' from the CSV
 
-csr_data=[] # Main data file
-exceptions = [] # Companies for which scraping errors encountered
+counter = 0
 
-# List of CINs to cycle through
-cinList = companies['cin'].tolist()
-
-# Define the scraping procedure
-
+#Define the scraping procedure
 def get_projects(cin):
-    try:
-        url = (starturl + str(i))
-        res = requests.get(url, headers=headers, timeout=7)
-        print('')
-        print('Connection status:', res.status_code, 'Company:', i)
 
-        if res.status_code == 200:
-            print('OK')
-        else:
-            print(i, 'returned status code', res.status_code)
+    # Is the CIN 21 characters starting with L or U?
+    if (len(i) != 21 or (i[0] != 'L' and i[0] != 'U')):
+        print('\n' + i + ' is not a valid CIN')
+        exceptions.append(i)
+
+    else:
+
+        # Main procedure:
+
+        try:
+
+            url = (starturl + str(i))
+            res = requests.get(url, headers=headers, timeout=7)
+            print('')
+            print('Connection status:', res.status_code, 'Company:', i)
+
+            if res.status_code == 200:
+                print('OK')
+
+            else:
+                print(i, ' returned status code ', res.status_code)
+
+            # Extract data tables of CSR projects
+
+            soup=bs4.BeautifulSoup(res.text, "html.parser")
+
+            # 2014-15
+
+            try:
+                csr14_15 = soup.find_all('div',{"id":"colfy1"})
+                tableRow14_15 = csr14_15[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
+
+                for tr in tableRow14_15:
+                    td = tr.find_all('td')
+                    row = [i.text for i in td][0:8]
+                    row.append('2014-15')
+                    row.append(i)
+                    if 'company_name' in companies.columns:
+                        row.append(companies['company_name'][counter])
+                    csr_data.append(row)
+
+            except:
+                print('No projects listed for 2014-15')
+
+            # 2015-16
+
+            try:
+                csr15_16 = soup.find_all('div',{"id":"colfy2"})
+                tableRow15_16 = csr15_16[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
+
+                for tr in tableRow15_16:
+                    td = tr.find_all('td')
+                    row = [i.text for i in td][0:8]
+                    row.append('2015-16')
+                    row.append(i)
+                    if 'company_name' in companies.columns:
+                        row.append(companies['company_name'][counter])
+                    csr_data.append(row)
+
+            except:
+                print('No projects listed for 2015-16')
+
+            # 2016-17
+
+            try:
+                csr16_17 = soup.find_all('div',{"id":"colfy3"})
+                tableRow16_17 = csr16_17[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
+
+                for tr in tableRow16_17:
+                    td = tr.find_all('td')
+                    row = [i.text for i in td][0:8]
+                    row.append('2016-17')
+                    row.append(i)
+                    if 'company_name' in companies.columns:
+                        row.append(companies['company_name'][counter])
+                    csr_data.append(row)
+
+            except:
+                print('No projects listed for 2016-17')
+
+        except:
+            print('Error encountered for CIN', i)
             exceptions.append(i)
-    except:
-        print('')
-        print('Results for', i, 'returned an error.')
-        exceptions.append(i)
 
-    try:
-
-        # Extract data tables of CSR projects
-
-        soup=bs4.BeautifulSoup(res.text, "html.parser")
-
-        # 2014-15
-
-        try:
-            csr14_15 = soup.find_all('div',{"id":"colfy1"})
-            tableRow14_15 = csr14_15[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
-
-            for tr in tableRow14_15:
-                td = tr.find_all('td')
-                row = [i.text for i in td][0:8]
-                row.append('2014-15')
-                row.append(i)
-                csr_data.append(row)
-
-        except:
-            print('2014-15 results not found')
-
-        # 2015-16
-
-        try:
-            csr15_16 = soup.find_all('div',{"id":"colfy2"})
-            tableRow15_16 = csr15_16[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
-
-            for tr in tableRow15_16:
-                td = tr.find_all('td')
-                row = [i.text for i in td][0:8]
-                row.append('2015-16')
-                row.append(i)
-                csr_data.append(row)
-
-        except:
-            print('2015-16 results not found')
-
-        # 2016-17?
-
-        try:
-            csr16_17 = soup.find_all('div',{"id":"colfy3"})
-            tableRow16_17 = csr16_17[0].find_all('table',{"id":"datatable"})[0].find_all('tr')[1:-1]
-
-            for tr in tableRow16_17:
-                td = tr.find_all('td')
-                row = [i.text for i in td][0:8]
-                row.append('2016-17')
-                row.append(i)
-                csr_data.append(row)
-
-        except:
-            print('2016-17 results not found')
-
-    except:
-        print('Error encountered for CIN', i)
-        exceptions.append(i)
-
-    time.sleep(1) # Wait one second between requests
+    # Wait one second between requests
+    time.sleep(1)
 
 # Run the scraping procedure
-for i in cinList[0:99]: # Companies ranked 1-100 in CSR spending
-    get_projects(i)
+# Run in test mode if -t argument provided at command line
+if (len(argv) == 3 and argv[2] == '-t'):
+    print("Test mode enabled: gathering up to first ten companies")
+    if len(cinList) > 10:
+        for i in cinList[0:9]:
+            get_projects(i)
+            counter += 1
+    else:
+        for i in cinList:
+            get_projects(i)
+            counter += 1
 
-# Create Pandas dataframe
+# Otherwise run in normal mode to collect data for all CINs in the CSV
+else:
+    for i in cinList:
+        get_projects(i)
+        counter += 1
 
-cresults=pd.DataFrame(csr_data,columns=['id', 'proj_name', 'proj_sector', 'state', 'district', 'outlay', 'spent', 'mode', 'year', 'CIN'])
+# Create Pandas dataframe to store the results
 
-# Save output (results and list of errors) as csv files
+if 'company_name' in companies.columns:
+    cresults=pd.DataFrame(csr_data,columns=['id', 'proj_name', 'proj_sector', 'state', 'district', 'outlay', 'spent', 'mode', 'year', 'cin', 'company_name'])
+
+else:
+    cresults=pd.DataFrame(csr_data,columns=['id', 'proj_name', 'proj_sector', 'state', 'district', 'outlay', 'spent', 'mode', 'year', 'cin'])
+
+# Save the dataframe as a timestamped CSV file
 
 timestamp = time.strftime("%Y%m%d-%H%M%S")
 cresults.to_csv('results_' + timestamp + '.csv', na_rep='.')
 print('Results written to disk. Timestamp:',timestamp)
 
-# Save list of exceptions/errors, if there were any
+# Save a list of exceptions/errors, if there were any
 
 if len(exceptions) > 0:
-    exceptionsTable=pd.DataFrame(exceptions,columns=['CIN'])
+    exceptionsTable=pd.DataFrame(exceptions,columns=['cin'])
     exceptionsTable.to_csv('exceptions_' + timestamp + '.csv', na_rep='.')
-    print('Exceptions file written to disk', len(exceptions), 'exceptions noted.')
+    print('Exceptions file written to disk:', len(exceptions), 'exceptions noted.')
 else:
     print('No exceptions/errors noted')
+
+exit(0)
